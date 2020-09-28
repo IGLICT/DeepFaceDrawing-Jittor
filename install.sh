ae7cef7d95af4c71d3d7dfb36524ce6ff28cbb4c
Shuyu Chen <chenshuyu@ict.ac.cn>
2020-09-28 15:38:22 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # install necessary library
-pip install pyqt5
-pip install Pillow 
-pip install scipy 
-pip install dominate 
-pip install opencv-python
+pip install pyqt5==5.9.2
+pip install Pillow
+pip install scipy
+pip install dominate
+pip install opencv-python==4.1.0.25
